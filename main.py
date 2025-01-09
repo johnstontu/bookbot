3d380165c6da9282c066027eca6afc10bb7233f9
johnstontu <tuckerjohnston13@gmail.com>
2025-01-08 19:44:23 -0500

    report output

--- main.py
+++ main.py
@@ -3,18 +3,26 @@ def main():
     text = get_book_text(book_path)
     chars = get_characters(text)
     char_dict = make_char_dict(chars)
-    print(char_dict)
-    
+    sorted_char_dict = sorted(char_dict.items(), key=lambda x: x[1], reverse=True)
+    word_count = get_word_count(text)
+    print("--- begin report of books/frenkenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    print()
+    for key, count in sorted_char_dict:
+        print(f"The {key} character was found {count} times")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_words(text):
-    return text.split()
+def get_word_count(text):
+    return len(text.split())
 
 def get_characters(text):
-    return list(text.lower())
+    abc = "qwertyuiopasdfghjklzxcvbnm"
+    letters =  list(text.lower())
+    filtered_letters = [item for item in letters if item in abc]
+    return filtered_letters
 
 def make_char_dict(chars):
     char_dict = {}
@@ -24,6 +32,6 @@ def make_char_dict(chars):
         else:
             char_dict[char] = 1
     return char_dict
-
+    
 
 main()
